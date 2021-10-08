45ca5b0ac4fec377e9e91e8c664f424286cbab8d
Mauvecrow <64569782+mauvecrow@users.noreply.github.com>
2021-10-08 11:57:44 -0600

    1- main: set CategoriesScreen as the new home page 2- categories_screen: added Scaffold to wrap GridView

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -8,17 +8,22 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DeliMeal'),
       ),
-      children: dummyCateogries
-          .map((catData) =>
-              CategoryItem(title: catData.title, color: catData.color))
-          .toList(),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: dummyCateogries
+            .map((catData) =>
+                CategoryItem(title: catData.title, color: catData.color))
+            .toList(),
+      ),
     );
   }
 }

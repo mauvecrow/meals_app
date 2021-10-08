280363199f417bef0fc086cd1a7a285409f5bde1
Mauvecrow <64569782+mauvecrow@users.noreply.github.com>
2021-10-08 14:41:06 -0600

    refactor based on name change

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import './dummy_data.dart';
-import './categories_item.dart';
+import 'category_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({Key? key}) : super(key: key);

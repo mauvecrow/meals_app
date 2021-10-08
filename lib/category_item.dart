f7d27d3f0e281d7858a4cf4642c4c853e7e440fb
Mauvecrow <64569782+mauvecrow@users.noreply.github.com>
2021-10-08 14:40:04 -0600

    renamed categories_item > category_item

---
+++ lib/category_item.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import './category_meals_screen.dart';
+
+class CategoryItem extends StatelessWidget {
+  final String title;
+  final Color color;
+
+  const CategoryItem({Key? key, required this.title, required this.color})
+      : super(key: key);
+
+  void selectCategory(BuildContext ctx) {
+    Navigator.of(ctx).push(MaterialPageRoute(
+      builder: (_) => const CategoryMealsScreen(),
+    ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => selectCategory(context),
+      splashColor: Theme.of(context).colorScheme.secondary,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.headline6,
+        ),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [color.withOpacity(0.7), color],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(15),
+        ),
+      ),
+    );
+  }
+}

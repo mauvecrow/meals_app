2871a391d3647ae96698e99ec94df7d9ae4a8184
Mauvecrow <64569782+mauvecrow@users.noreply.github.com>
2021-10-09 16:16:40 -0600

    updated meals screen to display corresponding meals based on dummy data's category values

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './dummy_data.dart';
+
 class CategoryMealsScreen extends StatelessWidget {
   static const routeName = '/category-meals';
   const CategoryMealsScreen({Key? key}) : super(key: key);
@@ -10,16 +12,19 @@ class CategoryMealsScreen extends StatelessWidget {
         ModalRoute.of(context)?.settings.arguments as Map<String, String>;
     final categoryId = routeArgs['id'];
     final categoryTitle = routeArgs['title'] as String;
+    final categoryMeals = DUMMY_MEALS
+        .where((meal) => meal.categories.contains(categoryId))
+        .toList();
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(categoryTitle),
-      ),
-      body: const Center(
-        child: Text(
-          'The recipes for the category!',
+        appBar: AppBar(
+          title: Text(categoryTitle),
         ),
-      ),
-    );
+        body: ListView.builder(
+          itemBuilder: (ctx, index) {
+            return Text(categoryMeals[index].title);
+          },
+          itemCount: categoryMeals.length,
+        ));
   }
 }

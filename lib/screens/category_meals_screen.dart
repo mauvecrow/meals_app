f03a903d7ea32d682c33fd9f861520cbccbefaf6
Mauvecrow <64569782+mauvecrow@users.noreply.github.com>
2021-10-09 16:41:37 -0600

    Create MealItem widget updated meals screen to use meal item

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../widgets/meal_item.dart';
 
 import '../dummy_data.dart';
 
@@ -22,7 +23,13 @@ class CategoryMealsScreen extends StatelessWidget {
         ),
         body: ListView.builder(
           itemBuilder: (ctx, index) {
-            return Text(categoryMeals[index].title);
+            var meal = categoryMeals[index];
+            return MealItem(
+                title: meal.title,
+                imageUrl: meal.imageUrl,
+                duration: meal.duration,
+                affordability: meal.affordability,
+                complexity: meal.complexity);
           },
           itemCount: categoryMeals.length,
         ));
